854aaef92e5ec0a7dbd9f70999b6a5d99f78b9b2
riverside <riverside96@protonmail.com>
2022-03-17 23:20:28 +0000

    Initial commit

---
+++ homepage.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include'navigation/navbar.php';  ?>
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+</head>
+<body>
+
+<div class="background-image"></div>
+
+
+</body>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .background-image {
+      background-image: url('images/finchart.jpg');
+      background-size: cover;
+      background-repeat: no-repeat;
+      height: 100vh;
+    }
+</style>
+</html>
---
+++ navigation/navbar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Navbar</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="navigation/navbar.css">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <header>
+            <a class="logo" href="/"><img src="images/logo.svg" width="45" height="45" alt="logo"></a>
+            <a class="logo2" href="/"><img src="images/alpha.svg" width="60" height="60" alt="logo"></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Books</a></li>
+                    <li><a href="#">Customer Registration</a></li>
+                    <li><a href="#">Issue/Return</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="#">Logout</a>
+            <p class="menu cta">Menu</p>
+        </header>
+        <div class="overlay">
+            <a class="close">&times;</a>
+            <div class="overlay__content">
+                <a href="#">Services</a>
+                <a href="#">Projects</a>
+                <a href="#">About</a>
+            </div>
+        </div>
+
+    </body>
+</html>
